02bc7a3eb7346ae33621ab9b3e8d0029142d6086
luanaassisi <luanaaraujodeassis@outlook.com>
2022-06-24 11:20:46 -0300

    Cifra de Cesar

--- assets/js/codificador.js
+++ assets/js/codificador.js
@@ -1,11 +1,64 @@
-let selecao = document.querySelector('#escolha')
-let cahvePasso = document.querySelector('#incremento')
 
-selecao.addEventListener('change', function(){
-    if(selecao.value == 'cifra'){
-        chavePasso.style.display = 'inline'
-    }else{
-        chavePasso.style.display = 'none'
+function codificar() {
+  let texto = document.getElementById("texto").value;
+  let numDesloc = parseInt(document.getElementById("deslocamento").value);
+
+  document.getElementById("textocifrado").innerHTML = cifrar(texto, numDesloc);
+}
+
+function cifrar(texto, numDesloc) {
+  let textoArray = [];
+  let deslocamentoArray = [];
+  let textoCodificado = "";
+
+    for (let i = 0; i < texto.length; i++){
+      textoArray.push(texto.charCodeAt(i));
     }
-})
 
+    for (j = 0; j < textoArray.length; j++){
+       if (textoArray[j] >= 65 && textoArray[j] <= 90){
+        deslocamentoArray.push((((textoArray[j] - 65 + numDesloc) % 26) + 65));
+      } else if (textoArray[j] >= 97 && textoArray[j] <= 122){
+        deslocamentoArray.push((((textoArray[j] - 97 + numDesloc) % 26) + 97));
+      } else {
+        deslocamentoArray.push(textoArray[j]);
+        }
+      }
+
+    for (g = 0; g < deslocamentoArray.length; g++){
+        textoCodificado += String.fromCharCode(deslocamentoArray[g]);
+      }
+    return textoCodificado;
+}
+
+function decodificar() {
+  let texto = document.getElementById("texto").value;
+  let numDesloc = parseInt(document.getElementById("deslocamento").value);
+
+  document.getElementById("textocifrado").innerHTML = cypherDec(texto, numDesloc);
+}
+
+function cypherDec(texto, numDesloc) {
+  let textoArray = [];
+  let deslocamentoArray = [];
+  let textoCodificado = "";
+
+    for (let i= 0; i < texto.length; i++){
+      textoArray.push(texto.charCodeAt(i));
+    }
+
+    for (j = 0; j < textoArray.length; j++){
+    if(textoArray[j] >= 65 && textoArray [j] <= 90){
+      deslocamentoArray.push((((textoArray[j] - 90 - numDesloc) % 26) +90));
+      }else if (textoArray[j] >= 97 && textoArray[j] <= 122){
+        deslocamentoArray.push((((textoArray[j] - 122 - numDesloc) % 26) + 122));
+      }else{
+        deslocamentoArray.push(textoArray[j]);
+        }
+      }
+
+    for (g = 0; g < deslocamentoArray.length; g++){
+        textoCodificado += String.fromCharCode(deslocamentoArray[g]);
+      }
+    return textoCodificado;
+}
